4d8bf401c425bbf0f2020af192a2c878df72c115
DenetsTim <sasha.tim.krutoi@gmail.com>
2023-09-04 20:25:32 +0000

    Final

--- handlers.py
+++ handlers.py
@@ -8,7 +8,11 @@ import kb, text
 
 router = Router()
 beauty_text = open('data/beauty/text.txt', 'r', encoding="utf-8").read().split('-')
-photo = FSInputFile("photo.png")
+photo = FSInputFile("photo.jpg")
+
+second_photo = FSInputFile("Second.png")
+end_photo = FSInputFile("end.png")
+sad_end_photo = FSInputFile("sad_end.jpg")
 
 @router.message(Command('start'))
 async def start_handler(msg: Message):
@@ -20,8 +24,14 @@ async def start_handler(msg: Message):
 async def menu(msg: Message):
     await msg.answer(text.menu, reply_markup=kb.menu)
 
+@router.callback_query(F.data == "menu")
+async def callback_menu(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer(text.menu, reply_markup=kb.menu)
+
 @router.callback_query(F.data == 'beauty')
 async def beauty(clbck: CallbackQuery):
+    await clbck.answer()
     message = await clbck.message.answer(beauty_text[0])
     for i in range(1, len(beauty_text)):
         sleep(0.1)
@@ -30,12 +40,46 @@ async def beauty(clbck: CallbackQuery):
 
 @router.callback_query(F.data == 'start_quest')
 async def start_quest(clbck: CallbackQuery):
+    await clbck.answer()
     await clbck.message.answer(text.first_question, reply_markup=kb.first_question)
 
 @router.callback_query(F.data == 'second_question')
 async def second_question(clbck: CallbackQuery):
-    await clbck.message.answer("Nice!")
+    await clbck.answer()
+    await clbck.message.answer_photo(second_photo, text.second_question, reply_markup=kb.second_question)
+
+@router.callback_query(F.data == "third_question")
+async def third_question(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer(text.third_question, reply_markup=kb.third_question)
+
+@router.callback_query(F.data == "fourth_question")
+async def fourth_question(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer(text.fourth_question, reply_markup=kb.fourth_question)
+
+@router.callback_query(F.data == "fifth_question")
+async def fifth_question(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer(text.fifth_question, reply_markup=kb.fifth_question)
+
+@router.callback_query(F.data == "end_quest")
+async def end_quest(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer_photo(end_photo, text.end_quest, reply_markup=kb.iexit_kb)
+
+@router.callback_query(F.data == "sad_end_quest")
+async def sad_end_quest(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer_photo(sad_end_photo, text.sad_end_quest, reply_markup=kb.iexit_kb)
+
 
 @router.callback_query(F.data == 'wrong_answer')
 async def wrong_answer(clbck: CallbackQuery):
-    await clbck.message.answer("Неа", reply_markup=kb.exit_kb)
+    await clbck.answer()
+    await clbck.message.answer(text.wrong_answer, reply_markup=kb.restart_quest)
+
+@router.callback_query(F.data == "secret_answer")
+async def secret_answer(clbck: CallbackQuery):
+    await clbck.answer()
+    await clbck.message.answer(text.secret_answer, reply_markup=kb.iexit_kb)
--- kb.py
+++ kb.py
@@ -7,6 +7,10 @@ menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
 iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
 
+restart_quest = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Сначала", callback_data='start_quest')]
+])
+
 first_question = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Йа", callback_data="second_question"),
     InlineKeyboardButton(text="Я", callback_data="second_question")],
@@ -14,4 +18,42 @@ first_question = InlineKeyboardMarkup(inline_keyboard=[
     InlineKeyboardButton(text="Че", callback_data="second_question")],
     [InlineKeyboardButton(text="Не я", callback_data="second_question"),
     InlineKeyboardButton(text="Гей", callback_data="wrong_answer")]
+])
+second_question = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Негры", callback_data="wrong_answer"),
+    InlineKeyboardButton(text="Белый", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="Солнце", callback_data="wrong_answer"),
+    InlineKeyboardButton(text="Луна", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="Звезда", callback_data="wrong_answer"),
+    InlineKeyboardButton(text="Метеорит", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="Небо", callback_data="wrong_answer"),
+    InlineKeyboardButton(text="Газон", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="Хромосома", callback_data="third_question"),
+    InlineKeyboardButton(text="Курсор", callback_data="wrong_answer")]
+])
+third_question = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="СЭККС", callback_data="fourth_question"),
+    InlineKeyboardButton(text="Стоны", callback_data="fourth_question")],
+    [InlineKeyboardButton(text="Порно", callback_data="fourth_question"),
+    InlineKeyboardButton(text="Релакс", callback_data="fourth_question")],
+    [InlineKeyboardButton(text="Сериал", callback_data="fourth_question"),
+    InlineKeyboardButton(text="Под наркотой", callback_data="fourth_question")],
+    [InlineKeyboardButton(text="ЖМЖ", callback_data="fourth_question"),
+    InlineKeyboardButton(text="МЖМ", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="Мне нужен только ты", callback_data="wrong_answer")]
+])
+fourth_question = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="-∞", callback_data="fifth_question"),
+    InlineKeyboardButton(text="+∞", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="0", callback_data="wrong_answer"),
+    InlineKeyboardButton(text="1", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="Че", callback_data="fifth_question"),
+    InlineKeyboardButton(text="Это всё?(", callback_data="fifth_question")]
+])
+fifth_question = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="👍", callback_data="end_quest"),
+    InlineKeyboardButton(text="👎", callback_data="wrong_answer")],
+    [InlineKeyboardButton(text="❤️", callback_data="end_quest"),
+    InlineKeyboardButton(text="Хочу отсосать этому гению", callback_data="secret_answer")],
+    [InlineKeyboardButton(text="Хуйня, не делай больше", callback_data="sad_end_quest")]
 ])
